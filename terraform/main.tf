0a8508b09cda2ec61e6dfe44362ece6c1bd9f833
Ramesh <saravram@a078177d79a2.ant.amazon.com>
2022-02-16 12:24:30 -0500

    sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "saravram+sandboxaft@amazon.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "AFT"
+    ManagedOrganizationalUnit = "MyOU"
     SSOUserEmail              = "saravram@amazon.com"
     SSOUserFirstName          = "sarav"
     SSOUserLastName           = "ram"
@@ -24,31 +24,4 @@ module "sandbox" {
   }
 
   account_customizations_name = "sandbox"
-},
-module "devaft" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "saravram+devaft@amazon.com"
-    AccountName               = "dev-aft"
-    ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "saravram@amazon.com"
-    SSOUserFirstName          = "sarav"
-    SSOUserLastName           = "ram"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "devaft"
 }
