4973612e1905791b4ff61173b60c80af95d7d816
Ramesh <saravram@a078177d79a2.ant.amazon.com>
2022-02-16 12:16:47 -0500

    devaft account

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,31 @@ module "sandbox" {
   }
 
   account_customizations_name = "sandbox"
+},
+module "devaft" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "saravram+devaft@amazon.com"
+    AccountName               = "dev-aft"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "saravram@amazon.com"
+    SSOUserFirstName          = "sarav"
+    SSOUserLastName           = "ram"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "devaft"
 }
